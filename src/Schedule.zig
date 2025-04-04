77925ef37f845c1319f31f0d5721d19dca2e7691
Mike Will <myQwil@gmail.com>
2025-04-04 05:17:55 -0400

    add option for logarithmic scaling

--- src/Schedule.zig
+++ src/Schedule.zig
@@ -9,6 +9,7 @@ const Config = struct {
 	night: u15 = 1900,
 	dawn: [2]f32 = .{ 4, 6 },
 	dusk: [2]f32 = .{ 19, 21 },
+	logarithmic: bool = false,
 
 	inline fn init(mem: std.mem.Allocator, home: std.fs.Dir) !Config {
 		const file = try home.openFile(
@@ -27,32 +28,39 @@ const Slope = struct {
 	time: Range(f32),
 	m: f32,
 	b: u15,
+	log: bool,
 
 	fn Range(T: type) type { return struct {
 		lo: T,
 		hi: T,
 	};}
 
-	fn init(hour: Range(f32), kelvin: Range(u15)) Slope {
+	fn init(hour: Range(f32), kelvin: Range(u15), log: bool) Slope {
 		const time: Range(f32) = .{
 			.lo = hour.lo,
 			.hi = if (hour.hi < hour.lo) hour.hi + 24 else hour.hi,
 		};
 		const run: f32 = time.hi - time.lo;
 		if (run == 0) {
-			return .{ .time = time, .m = 0, .b = kelvin.hi };
+			return .{ .time = time, .m = 0, .b = kelvin.hi, .log = false };
 		} else {
-			const irise: i16 = @as(i16, kelvin.hi) - @as(i16, kelvin.lo);
-			const rise: f32 = @floatFromInt(irise);
-			return .{ .time = time, .m = rise / run, .b = kelvin.lo };
+			const hi: f32 = @floatFromInt(kelvin.hi);
+			const lo: f32 = @floatFromInt(kelvin.lo);
+			return .{
+				.time = time,
+				.m = if (log) @log2(hi / lo) / run else (hi - lo) / run,
+				.b = kelvin.lo,
+				.log = log,
+			};
 		}
 	}
 
 	fn at(self: *const Slope, hour: f32) u15 {
 		const t = &self.time;
 		const x: f32 = @min(if (hour < t.lo) hour + 24 else hour, t.hi) - t.lo;
-		const kelvin: f32 = @round(self.m * x + @as(f32, @floatFromInt(self.b)));
-		return @intFromFloat(kelvin);
+		const b: f32 = @floatFromInt(self.b);
+		return @intFromFloat(@round(
+			if (self.log) @exp2(self.m * x) * b else self.m * x + b));
 	}
 };
 
@@ -61,11 +69,13 @@ pub fn init(mem: std.mem.Allocator, home: std.fs.Dir) Schedule {
 	return .{
 		.dawn = .init(
 			.{ .lo = cfg.dawn[0], .hi = cfg.dawn[1] },
-			.{ .lo = cfg.night, .hi = cfg.day }
+			.{ .lo = cfg.night, .hi = cfg.day },
+			cfg.logarithmic,
 		),
 		.dusk = .init(
 			.{ .lo = cfg.dusk[0], .hi = cfg.dusk[1] },
-			.{ .lo = cfg.day, .hi = cfg.night }
+			.{ .lo = cfg.day, .hi = cfg.night },
+			cfg.logarithmic,
 		),
 	};
 }
