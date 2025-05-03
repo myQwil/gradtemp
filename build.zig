dc680e1a7860c007aa4c6b48d225317a6c7db2b6
Mike Will <myQwil@gmail.com>
2025-05-03 08:36:09 -0400

    remove exe_check

--- build.zig
+++ build.zig
@@ -23,11 +23,4 @@ pub fn build(b: *std.Build) void {
 	}
 	const run_step = b.step("run", "Run the main app");
 	run_step.dependOn(&run_cmd.step);
-
-	const exe_check = b.addExecutable(.{
-		.name = "gradtemp",
-		.root_module = exe_mod,
-	});
-	const check = b.step("check", "Check if gradtemp compiles");
-	check.dependOn(&exe_check.step);
 }
